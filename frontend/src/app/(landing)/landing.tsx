50c6cf03eb20c80334a83e3232706056d83bea37
SiddharthaWork <siddharthashresthasir@gmail.com>
2025-05-21 22:20:40 +0545

    Setting Up

---
+++ frontend/src/app/(landing)/landing.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div className='bg-red-500'>
+Hello
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Landing
