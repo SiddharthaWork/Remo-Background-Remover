6b6a615c30661ac3318a2c505b63c3dc8cb87ec5
SiddharthaWork <siddharthashresthasir@gmail.com>
2025-06-10 22:26:58 +0545

    Worked on Slider

--- frontend/src/components/Slider.tsx
+++ frontend/src/components/Slider.tsx
@@ -16,11 +16,11 @@ const Slider = () => {
       <h1 className='text-5xl font-bold mt-10 text-center'>Remove Background with <br /> <span className='text-[#61db3f]'>100% </span> Accuracy</h1>
 
       {/* Image */}
-      <div>
-        <img src="https://media.architecturaldigest.com/photos/637949b3407644b8cdc8947f/16:9/w_2560%2Cc_limit/1442809583" style={{ clipPath: `inset(0 ${100.2 - slider}% 0 0)`}} alt="" />
-        <img src="https://w7.pngwing.com/pngs/83/423/png-transparent-messi-thumbnail.png" style={{ clipPath: `inset(0 0 0 ${slider}%  )`}} alt="" />
+      <div className='w-4xl rounded-xl mx-auto mt-10 relative overflow-hidden'>
+        <img src="/image_w_bg.png" style={{ clipPath: `inset(0 ${100.2 - slider}% 0 0)`}} alt="" />
+        <img className='absolute top-0 left-0' src="image_wo_bg.png" style={{ clipPath: `inset(0 0 0 ${slider}%  )`}} alt="" />
 
-        <input type="range" min={0} max={100} value={slider} onChange={handlerSlider} className='w-full h-2 bg-gray-200 rounded-lg cursor-pointer dark:bg-gray-700' />
+        <input type="range" min={0} max={100} value={slider} onChange={handlerSlider} className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10' />
       </div>
     </div>
   )
