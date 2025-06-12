91fd814e93d7d595a7f39a74ff75368a82a5b59c
SiddharthaWork <siddharthashresthasir@gmail.com>
2025-06-12 22:35:17 +0545

    Completed Slider

--- frontend/src/components/Slider.tsx
+++ frontend/src/components/Slider.tsx
@@ -1,4 +1,3 @@
-
 'use client'
 import React from 'react'
 import { useState } from 'react'
@@ -17,10 +16,11 @@ const Slider = () => {
 
       {/* Image */}
       <div className='w-4xl rounded-xl mx-auto mt-10 relative overflow-hidden'>
-        <img src="/image_w_bg.png" style={{ clipPath: `inset(0 ${100.2 - slider}% 0 0)`}} alt="" />
-        <img className='absolute top-0 left-0' src="image_wo_bg.png" style={{ clipPath: `inset(0 0 0 ${slider}%  )`}} alt="" />
-
-        <input type="range" min={0} max={100} value={slider} onChange={handlerSlider} className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10' />
+        <img src="/image_w_bg.png" className='border-r-0 border-white outline-none' style={{ clipPath: `inset(0 ${100.2 - slider}% 0 0)`}} alt="" />
+        <img className='absolute top-0 left-0 border-l-0 border-white outline-none h-full' src="image_wo_bg.png"  style={{ clipPath: `inset(0 0 0 ${slider}%  )`}} alt="" />
+      {/* Slider */}
+        <input type="range" value={slider} onChange={handlerSlider} min={0} max={100}
+        className='absolute w-full top-1/2 left-1/2 transform -translate-x-[52%] -translate-y-1/2 z-10 slider bg-black outline-none' />
       </div>
     </div>
   )
